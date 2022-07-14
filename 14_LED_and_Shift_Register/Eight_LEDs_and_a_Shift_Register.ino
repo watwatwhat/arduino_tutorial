7a73821870a939f28761d99abbfa3e4eadd368c6
Tanaka <takayuki.tanaka@sap.com>
2022-07-14 15:49:15 +0900

    update

---
+++ 14_LED_and_Shift_Register/Eight_LEDs_and_a_Shift_Register.ino
@@ -0,0 +1,36 @@
+/*
+ * Author: MIUZEI
+ * IDE V1.6.9
+ * Email:support@miuzeipro.com 
+ */
+int latchPin = 5;
+int clockPin = 6;
+int dataPin = 4; 
+byte leds = 0;
+ 
+void setup() 
+{
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+}
+void loop() 
+{
+  leds = 0;
+  updateShiftRegister();
+  delay(500);
+  for (int i = 0; i < 8; i++)
+  {
+    bitSet(leds, i);
+    updateShiftRegister();
+    delay(500);
+  }
+}
+ 
+void updateShiftRegister()
+{
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds);
+   digitalWrite(latchPin, HIGH);
+}
+
---
+++ 19_One_step_at_a_time/One_step_at_a_time.ino
@@ -0,0 +1,24 @@
+/*
+ * Author: MIUZEI
+ * IDE V1.6.9
+ * Email:support@miuzeipro.com
+ */
+#include <Stepper.h>
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
+// initialize the stepper library on pins 2 through 5:
+Stepper myStepper(stepsPerRevolution, 2, 3, 4, 5);
+int stepCount = 0;         // number of steps the motor has taken
+void setup() {
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+void loop() {
+  // step one step:
+  myStepper.step(1);
+  Serial.print("steps:");
+  Serial.println(stepCount);
+  stepCount++;
+  delay(500);
+}
+
